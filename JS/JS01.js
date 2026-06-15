2ae57fe53ad1c989c86d4785f0a29386bd4a5ae4
Rapha-surdo <raphaell.souza@edu.senai.br>
2026-06-15 15:50:51 -0300

    JS3

--- JS/JS01.js
+++ JS/JS01.js
@@ -3,5 +3,6 @@ function apresentar() {
     var entrada = prompt("Salário:"); 
     var entrada2 = prompt("Vendas:"); 
     var comissao = Number(entrada2) * 0.025; 
-    alert("Oi, eu sou " + nome + ", meu salário é R$ " + entrada + ", minhas vendas foram de R$ " + entrada2 + " e a comissão é R$ " + comissao + "."); 
+    var total = Number(comissao) + Number(entrada); 
+    alert("Oi, eu sou " + nome + ", meu salário é R$ " + entrada + ", minhas vendas foram de R$ " + entrada2 + " e a comissão é R$ " + comissao + ". Sendo no total R$" + total); 
 }
